af7a6523c704569c2f8f6db0097b01d95fe14d4f
Aykhan Huseyn <immense_ideas@icloud.com>
2022-08-12 19:17:34 +0400

    feat: added comment

--- src/index.js
+++ src/index.js
@@ -4,12 +4,14 @@ import { BrowserRouter } from 'react-router-dom';
 import './styles/index.css';
 import App from './App';
 
+// this will not conflict
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>
+	<React.StrictMode>
+		<BrowserRouter>
+			<App />
+		</BrowserRouter>
+	</React.StrictMode>
 );
